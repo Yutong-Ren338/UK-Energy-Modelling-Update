904e59c6e6cb0f54b93771f2b7ef7cfa8ca1919f
Sam <sam.van.stroud@cern.ch>
2025-07-16 11:50:23 +0100

    add test

--- tests/test_supply_model.py
+++ tests/test_supply_model.py
@@ -26,3 +26,23 @@ def test_fraction_days_without_excess() -> None:
     plt.legend()
     plt.savefig(OUTPUT_DIR / "fraction_days_without_excess.png")
     plt.close()
+
+
+def test_fraction_days_without_excess_naive_demand() -> None:
+    A.Nuclear.Capacity = 12 * U.GW
+    naive_nuclear = supply_model.fraction_days_without_excess("era5", return_mean=True, naive_demand_scaling=True)
+    new_nuclear = supply_model.fraction_days_without_excess("era5", return_mean=True, naive_demand_scaling=False)
+    A.Nuclear.Capacity = 0 * U.GW
+    naive_no_nuclear = supply_model.fraction_days_without_excess("era5", return_mean=True, naive_demand_scaling=True)
+    new_no_nuclear = supply_model.fraction_days_without_excess("era5", return_mean=True, naive_demand_scaling=False)
+
+    plt.figure()
+    plt.plot(naive_nuclear.index.values, naive_nuclear, label="Naive 12 GW Nuclear")
+    plt.plot(naive_no_nuclear.index.values, naive_no_nuclear, label="Naive 0 GW Nuclear")
+    plt.plot(new_nuclear.index.values, new_nuclear, ls="--", label="Seasonal 12 GW Nuclear", color="blue")
+    plt.plot(new_no_nuclear.index.values, new_no_nuclear, ls="--", label="Seasonal 0 GW Nuclear", color="orange")
+    plt.xlabel("Renewable Capacity (GW)")
+    plt.ylabel("Days without Excess Generation")
+    plt.legend()
+    plt.savefig(OUTPUT_DIR / "fraction_days_without_excess_naive_demand.png")
+    plt.close()
